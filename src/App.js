ad7d90f406a241c78d3ce38ecb1a24f88dd5ba99
DCull1984 <nowe2111@yahoo.co.uk>
2019-02-04 17:43:01 +0000

    updated front end

--- src/App.js
+++ src/App.js
@@ -7,7 +7,19 @@ class App extends Component {
   render() {
 	  return(
 	
-	<div>	
+	<div>
+		<header>
+			<ul className="Page_Links">
+				<li><a href="#Home">Home</a></li>
+				<li><a href="./Trainee_Ratings.js">Trainee Rating</a></li>
+				<li><a href="#Holiday">Trainers on Holiday</a></li>
+				<li><a href="#TrainerSkills">Trainers Skills</a></li>
+				<li><a href="#TraineeSchedule">Trainee Schedule</a></li>
+				<li><a href="#TrainerSubject">Trainers Subject</a></li>
+			</ul>
+		</header>
+		
+		
 		<TraineeRatings />
 	</div>
 		);
