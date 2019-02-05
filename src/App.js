2691c84fc26757f92f4cbf7b1312f01716176a53
DCull1984 <nowe2111@yahoo.co.uk>
2019-02-05 15:06:46 +0000

    Syntax adjustments with ESLint

--- src/App.js
+++ src/App.js
@@ -2,28 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 import TraineeRatings from './Trainee_Ratings.js';
 
-
 class App extends Component {
-  render() {
-	  return(
-	
-	<div>
-		<header>
-			<ul className="Page_Links">
-				<li><a href="#Home">Home</a></li>
-				<li><a href="./Trainee_Ratings.js">Trainee Rating</a></li>
-				<li><a href="#Holiday">Trainers on Holiday</a></li>
-				<li><a href="#TrainerSkills">Trainers Skills</a></li>
-				<li><a href="#TraineeSchedule">Trainee Schedule</a></li>
-				<li><a href="#TrainerSubject">Trainers Subject</a></li>
-			</ul>
-		</header>
-		
-		
-		<TraineeRatings />
-	</div>
-		);
-	}
+ render() {
+  return(
+
+   <div>
+
+    <header>
+     <ul className="Page_Links">
+      <li><a href="#Home">Home</a></li>
+      <li><a href="./Trainee_Ratings.js">Trainee Rating</a></li>
+      <li><a href="#Holiday">Trainers on Holiday</a></li>
+      <li><a href="#TrainerSkills">Trainers Skills</a></li>
+      <li><a href="#TraineeSchedule">Trainee Schedule</a></li>
+      <li><a href="#TrainerSubject">Trainers Subject</a></li>
+     </ul>
+    </header>
+
+    <TraineeRatings />
+   </div>
+  );
+ }
 }
 
 export default App;
--- src/Trainee_Ratings.js
+++ src/Trainee_Ratings.js
@@ -2,46 +2,46 @@ import React, { Component } from 'react';
 import './App.css'
 
 class TraineeRatings extends Component {
-  render() {
-    return (
-      <div className="Trainee_Ratings">
-        <div className="The_Rating_Table">
-		
-		<h2>Java Ratings</h2>
-		
-			<table className="scoreTable">
-				<h3>Rating Table</h3>
-				<tr>
-					<th>ID</th>
-					<th>Trainee Name</th>
-					<th>Ratings</th>
-				</tr>
-				
-				<tr>
-					<td>1</td>
-					<td>Joe Doe</td>
-					<td>8.5</td>
-				</tr>
-				
-				<tr>
-					<td>2</td>
-					<td>Jane Doe</td>
-					<td>3.0</td>
-				</tr>
-				
-				<tr>
-					<td>3</td>
-					<td>John Doe</td>
-					<td>10.0</td>
-				</tr>
-			</table>
-			
-			<button className="Collapse_Table">Hide Trainees</button>
-			
-		</div>
-      </div>
-    );
-  }
+ render() {
+  return (
+   <div className="Trainee_Ratings">
+    <div className="The_Rating_Table">
+
+     <h2>Java Ratings</h2>
+
+     <table className="scoreTable">
+      <h3>Rating Table</h3>
+       <tr>
+        <th>ID</th>
+        <th>Trainee Name</th>
+        <th>Ratings</th>
+       </tr>
+
+       <tr>
+        <td>1</td>
+        <td>Joe Doe</td>
+        <td>8.5</td>
+       </tr>
+
+       <tr>
+        <td>2</td>
+        <td>Jane Doe</td>
+        <td>3.0</td>
+       </tr>
+
+       <tr>
+        <td>3</td>
+        <td>John Doe</td>
+        <td>10.0</td>
+       </tr>
+     </table>
+
+     <button className="Collapse_Table">Hide Trainees</button>
+
+     </div>
+    </div>
+   );
+ }
 }
 
 export default TraineeRatings;
