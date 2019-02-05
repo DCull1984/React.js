f98a6cfdd810a37a26562da0f37de2c84047c50f
DCull1984 <nowe2111@yahoo.co.uk>
2019-02-05 17:23:55 +0000

    Adjusted colours removed button

--- src/App.js
+++ src/App.js
@@ -8,16 +8,7 @@ class App extends Component {
 
    <div>
 
-    <header>
-     <ul className="Page_Links">
-      <li><a href="#Home">Home</a></li>
-      <li><a href="./Trainee_Ratings.js">Trainee Rating</a></li>
-      <li><a href="#Holiday">Trainers on Holiday</a></li>
-      <li><a href="#TrainerSkills">Trainers Skills</a></li>
-      <li><a href="#TraineeSchedule">Trainee Schedule</a></li>
-      <li><a href="#TrainerSubject">Trainers Subject</a></li>
-     </ul>
-    </header>
+    
 
     <TraineeRatings />
    </div>
--- src/Trainee_Ratings.js
+++ src/Trainee_Ratings.js
@@ -5,9 +5,19 @@ class TraineeRatings extends Component {
  render() {
   return (
    <div className="Trainee_Ratings">
-    <div className="The_Rating_Table">
+    
+    <header>
+     <ul className="Page_Links">
+      <li><a href="#Home">Home</a></li>
+      <li><a class="active" href="./Trainee_Ratings.js">Trainee Rating</a></li>
+      <li><a href="#Holiday">Trainers on Holiday</a></li>
+      <li><a href="#TrainerSkills">Trainers Skills</a></li>
+      <li><a href="#TraineeSchedule">Trainee Schedule</a></li>
+      <li><a href="#TrainerSubject">Trainers Subject</a></li>
+     </ul>
+    </header>
 
-     <h2>Java Ratings</h2>
+    <h2>Java Ratings</h2>
 
      <table className="scoreTable">
       <h3>Rating Table</h3>
@@ -35,11 +45,10 @@ class TraineeRatings extends Component {
         <td>10.0</td>
        </tr>
      </table>
-
+     
      <button className="Collapse_Table">Hide Trainees</button>
 
      </div>
-    </div>
    );
  }
 }
